832c0acf3bf4c6962eb60fbd78a7451fd0fb0901
Mila <milamgz@gmail.com>
2024-01-16 12:45:12 +0100

    commit

--- WeatherAssignment.py
+++ WeatherAssignment.py
@@ -3,6 +3,7 @@ import json
 with open ("precipitation.json", encoding='utf-8') as file: 
     precipitation_list = json.load(file)
 
+#.1 
     station_code_seattle = "GHCND:US1WAKG0038" 
     
     seattle_measurements = []
@@ -12,21 +13,30 @@ with open ("precipitation.json", encoding='utf-8') as file:
             seattle_measurements.append(dic) 
             dic["date"] = dic["date"].split("-") 
     
+    #total precipitation per month
     total_per_month = {}
-    results_list = []
-    
+    results_monthly_list = []
     for dic in seattle_measurements:
         if dic["date"][1] in total_per_month:
             total_per_month[dic["date"][1]] = total_per_month[dic["date"][1]] + dic["value"]
         elif dic["date"][1] not in total_per_month: 
                total_per_month[dic["date"][1]] = dic["value"]
-    
-    for key, value in total_per_month.items():
-        results_list.append(value)
-    
-    with open("results.json", "w") as file:
-        json.dump(results_list, file, indent=3) 
-    
 
-        
+    for key, value in total_per_month.items():
+        results_monthly_list.append(value)
+
+#.2 
+    total_per_year = 0 
+    
+    for i in results_monthly_list: 
+        total_per_year = total_per_year + i
+
+    #relative monthly precipitation 
+    relative_monthly_precipitation = []
+    for m in results_monthly_list:
+        ratio = m / total_per_year
+        relative_monthly_precipitation.append(round(ratio, 2))
+    
+         
+
 
